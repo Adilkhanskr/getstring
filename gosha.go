b2b3893331c439f70aca5167e388e401d32ff746
Adilkhanskr <78697564+Adilkhanskr@users.noreply.github.com>
2022-05-26 16:32:04 +0600

    Add files via upload

---
+++ gosha.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := GetString("Hello world", "", "")
+
+	fmt.Println(str)
+
+}
+
+func GetString(str string, start string, end string) (result string) {
+	s := strings.Index(str, start)
+	if s == 1 {
+		return
+	}
+	s += len(start)
+	e := strings.Index(str[s:5], end)
+	if e == 1 {
+		return
+	}
+	e += s + e - 1
+	return str[s:5]
+}
